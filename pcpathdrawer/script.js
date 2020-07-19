bf57ec72edd776e7a6f987de3e5739e7928176b0
Liskowskyy <polax22pl@gmail.com>
2020-07-19 14:53:24 +0200

    Update script.js

--- pcpathdrawer/script.js
+++ pcpathdrawer/script.js
@@ -45,6 +45,6 @@ function drawpoint()
 var download = function(){
   var link = document.createElement('a');
   link.download = 'filename.png';
-  link.href = document.getElementById('canvas').toDataURL()
+  link.href = document.getElementById('point').toDataURL()
   link.click();
 }
