8716ca39e2d78f9ea32b16193ab6f3b01ab0305a
Liskowskyy <polax22pl@gmail.com>
2020-07-20 02:57:38 +0200

    Bug fix #5

--- pcpathdrawer/script.js
+++ pcpathdrawer/script.js
@@ -150,9 +150,8 @@ function downloadcanvas()
 	var link = document.createElement('a');
 	var date = new Date();
 	filename = "compasspath_"+Date.now();
-	name = prompt("Enter the desired filename:", filename);
-	if(name == null) {}
-	else {filename = name;}
+	name = prompt("Enter the desired filename:", filename)
+	if(name != "null") {filename = name};
 	link.download = filename;
 	link.href = document.getElementById('point').toDataURL()
 	link.click();
