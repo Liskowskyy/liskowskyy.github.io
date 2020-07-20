a34673911532c835b8368fc0edd2f9ac16ba88ca
Liskowskyy <polax22pl@gmail.com>
2020-07-21 00:35:53 +0200

    New approach to force reloading

--- pcpathdrawer/script.js
+++ pcpathdrawer/script.js
@@ -4,7 +4,7 @@ fromx = 0;
 fromy = 0;
 lastposx = 0;
 lastposy = 0;
-scriptver = "2020.07.21_00-33-00";
+scriptver = "2020.07.21_00-35-00";
 
 $(document).ready(function() {
   $.ajaxSetup({ cache: false });
@@ -31,7 +31,7 @@ function latestdatacheck()
 
 function forcereload()
 {
-	location.reload(true);
+	window.location.href = window.location.href
 }
 
 function reset()
