c6f50d3aa7694f6059e4389171a5937797ee4b0f
Liskowskyy <polax22pl@gmail.com>
2020-07-22 03:02:11 +0200

    Update script.js

--- videodatausage/script.js
+++ videodatausage/script.js
@@ -2,6 +2,26 @@ function getjson()
 {
 	$.getJSON('templates.json', function(data) 
 	{
-		alert(data);
+	group = document.getElementById("usertemplategroup").value;
+	template = document.getElementById("templateyt").value;
+    bitratemin = data.group.template.bitratemin;
+	bitratemax = data.group.template.bitratemax;
 	});
 }
+
+$( "#usertemplategroup" ).change(function() 
+{
+	templategroup = document.getElementById("usertemplategroup").value;
+	$('div[id^="div-template"]').hide();
+	if(templategroup == "yt")
+	{
+		$('div[id="div-templateyt"]').show();
+	}
+	if(templategroup == "ytlive")
+	{
+		$('div[id="div-templateytlive"]').show();
+	}
+	if(templategroup == "custom")
+	{
+	}
+});
