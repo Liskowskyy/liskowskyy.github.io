c045358c855fedd35bc1d703800fc5dd2d8803d9
Liskowskyy <polax22pl@gmail.com>
2020-07-22 22:55:17 +0200

    Update script.js

--- videodatausage/script.js
+++ videodatausage/script.js
@@ -55,10 +55,10 @@ function calcresult()
 	
 	if(includeminimum==true)
 	{
-		alert("Data used per minute: "MBperminutemin+"MB-"+MBperminutemax+"MB"+"\nData used per hour: "MBperhourmin+"MB-"+MBperhourmax+"MB";
+		alert("Data used per minute: "MBperminutemin+"MB-"+MBperminutemax+"MB"+"\nData used per hour: "MBperhourmin+"MB-"+MBperhourmax+"MB");
 	}
 	else
 	{
-		alert("Data used per minute: "+MBperminutemax+"MB"+"\nData used per hour: "+"+MBperhourmax+"MB";
+		alert("Data used per minute: "+MBperminutemax+"MB"+"\nData used per hour: "+MBperhourmax+"MB");
 	}
 }
