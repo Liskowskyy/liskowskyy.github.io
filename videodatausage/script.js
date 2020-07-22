4c0744322c1aaf24bba36dc07020975146735a89
Liskowskyy <polax22pl@gmail.com>
2020-07-22 02:08:39 +0200

    JSON testing

--- videodatausage/script.js
+++ videodatausage/script.js
@@ -1,4 +1,27 @@
-$( "#templategroup" ).change(function() 
+function getjson()
 {
-	templategroup = document.getElementById("templategroup".value);
+	$.getJSON('templates.json', function(data) 
+	{
+	group = "yt";
+	template = "2160p60";
+    bitratemin = data.group.template.bitratemin;
+	bitratemax = data.group.template.bitratemax;
+	}
+
+$( "#usertemplategroup" ).change(function() 
+{
+	templategroup = document.getElementById("usertemplategroup").value;
+	$('div[id^="div-template"]').hide();
+	if(templategroup == "yt")
+	{
+		$('div[id="div-templateyt"]').show();
+	}
+	if(templategroup == "ytlive")
+	{
+		$('div[id="div-templateytlive"]').show();
+	}
+	if(templategroup == "custom")
+	{
+		template
+	}
 });
