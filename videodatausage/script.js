248e30fcbe0b0d5ce35565d7f2c4d5e4fdc6388c
Liskowskyy <polax22pl@gmail.com>
2020-07-22 02:57:53 +0200

    Testing

--- videodatausage/script.js
+++ videodatausage/script.js
@@ -5,20 +5,3 @@ function getjson()
 		alert(data);
 	});
 }
-
-$( "#usertemplategroup" ).change(function() 
-{
-	templategroup = document.getElementById("usertemplategroup").value;
-	$('div[id^="div-template"]').hide();
-	if(templategroup == "yt")
-	{
-		$('div[id="div-templateyt"]').show();
-	}
-	if(templategroup == "ytlive")
-	{
-		$('div[id="div-templateytlive"]').show();
-	}
-	if(templategroup == "custom")
-	{
-	}
-});
