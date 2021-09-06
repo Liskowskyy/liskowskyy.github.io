4dcfe05b2096f7bf05454b8c873afe4b7a24e381
Liskowskyy <polax22pl@gmail.com>
2021-09-06 21:11:38 +0200

    Added "Gdzie WF?" site

---
+++ gdziewf/scripts.js
@@ -0,0 +1,21 @@
+function checkwf() {
+	var d = new Date();
+	alert();
+	if (d.getDay() == 0) {
+		if (d.getDate()%2 == 0) {
+			document.getElementById("answer").innerHTML = "Dzisiaj WF jest na małej sali lub boisku, gdyż pierwszy dzień tygodnia jest parzysty – " +d.getDate();
+		}
+		else {
+			document.getElementById("answer").innerHTML = "Dzisiaj WF jest na dużej sali, gdyż pierwszy dzień tygodnia jest nieparzysty – " +d.getDate();
+		}
+	}
+	else {
+		if ((d.getDate()-d.getDay()+1)%2 == 0) {
+			document.getElementById("answer").innerHTML = "Dzisiaj WF jest na małej sali lub boisku, gdyż pierwszy dzień tygodnia jest parzysty – " +d.getDate();
+		}
+		else {
+			document.getElementById("answer").innerHTML = "Dzisiaj WF jest na dużej sali, gdyż pierwszy dzień tygodnia jest nieparzysty – " +d.getDate();
+		}
+	}
+		
+	}
